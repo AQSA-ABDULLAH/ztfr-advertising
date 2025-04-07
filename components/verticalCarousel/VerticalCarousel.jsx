866ac9ef19647b6f150edd9d835b064ce95db618
Aqsa-Abdullah090 <aqsa.abdull@zimogroup.org>
2025-04-07 14:29:36 +0500

    add opacity in hover effect

--- components/verticalCarousel/VerticalCarousel.jsx
+++ components/verticalCarousel/VerticalCarousel.jsx
@@ -3,18 +3,20 @@ import data from "../data/brandColumns.json"; // update path as per your file lo
 
 const BrandScroller = ({ brands, animationClass }) => (
   <div className="relative overflow-hidden h-[100vh] w-[150px] 2xl:w-[200px]">
-    <div className={`scroll-track ${animationClass}`}>
-      {[...brands, ...brands].map((brand, idx) => (
-        <div key={`brand-${idx}`} className="brand-image">
-          <img
-            src={brand.src}
-            alt={`brand-${idx}`}
-            className="rounded-[12px] my-[12px] cursor-pointer transition-all duration-300"
-          />
-        </div>
-      ))}
-    </div>
+  <div className={`scroll-track ${animationClass} group`}>
+    {[...brands, ...brands].map((brand, idx) => (
+      <div key={`brand-${idx}`} className="brand-image">
+      <img
+  src={brand.src}
+  alt={`brand-${idx}`}
+  className="image-item rounded-[12px] my-[12px] cursor-pointer transition-all duration-300 group-hover:opacity-25 hover:!opacity-100"
+/>
+
+      </div>
+    ))}
   </div>
+</div>
+
 );
 
 const VerticalCarousel = () => {
@@ -30,7 +32,7 @@ const VerticalCarousel = () => {
   } = data;
 
   return (
-    <div className="flex gap-[16px] justify-around">
+   <div className="flex gap-[16px] justify-around group hover-effect-group">
       <BrandScroller brands={brandsColumn1} animationClass="scroll-up" />
       <BrandScroller brands={brandsColumn2} animationClass="scroll-down" />
       <BrandScroller brands={brandsColumn3} animationClass="scroll-up" />
