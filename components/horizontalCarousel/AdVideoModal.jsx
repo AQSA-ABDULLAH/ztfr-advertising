568fc59d22ed925636499500c60b3a97c2717148
Aqsa-Abdullah090 <aqsa.abdull@zimogroup.org>
2025-04-08 13:53:14 +0500

    View video when click on any poster

--- components/horizontalCarousel/AdVideoModal.jsx
+++ components/horizontalCarousel/AdVideoModal.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+"use client";
+
+import React, { useRef, useState, useEffect } from "react";
 import Image from "next/image";
 
 const AdVideoModal = ({ onClose }) => {
+  const videoRef = useRef(null);
+  const [isMuted, setIsMuted] = useState(true);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.muted = isMuted;
+    }
+  }, [isMuted]);
+
+  const toggleMute = () => {
+    setIsMuted((prev) => !prev);
+  };
+
   return (
     <div className="fixed bottom-24 lg:bottom-[unset] top-[unset] lg:top-1/2 lg:-translate-y-1/2 left-1/2 -translate-x-1/2 z-40 w-full lg:w-[800px] 3xl:w-[1196px] h-auto rounded-2xl overflow-hidden opacity-100">
       <video
+        ref={videoRef}
         src="https://firebasestorage.googleapis.com/v0/b/ztfr-ff6ab.appspot.com/o/advertVideos%2FHey%20Jude%20%20%20adidas.mp4?alt=media&token=831e2020-b7bd-49cd-b6fb-35bf91133bff"
         autoPlay
         loop
+        muted={isMuted}
+        playsInline
         className="w-full rounded-2xl"
-      ></video>
+      />
 
       <Image
         src="/assets/advertising/ZTFR PARTNER.svg"
@@ -40,13 +58,17 @@ const AdVideoModal = ({ onClose }) => {
         </svg>
       </button>
 
-      {/* Mute Button (no logic attached yet) */}
-      <button className="absolute top-16 right-7">
+      {/* Mute Button */}
+      <button
+        className="absolute top-16 right-7"
+        onClick={toggleMute}
+        aria-label="Toggle Mute"
+      >
         <Image
-          src="/assets/UnmuteVolume.svg"
+          src={isMuted ? "/assets/MuteVolume.svg" : "/assets/UnmuteVolume.svg"}
           width={25}
           height={25}
-          alt="Unmute"
+          alt={isMuted ? "Unmute" : "Mute"}
           className="w-[25px] h-[25px] cursor-pointer"
         />
       </button>
@@ -55,3 +77,4 @@ const AdVideoModal = ({ onClose }) => {
 };
 
 export default AdVideoModal;
+
