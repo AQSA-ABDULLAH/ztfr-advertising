c126efe2e69536167bea4b4f16ddb3a1605aeca7
Aqsa-Abdullah090 <aqsa.abdull@zimogroup.org>
2025-04-09 13:24:45 +0500

    slipt columns into half

--- components/verticalCarousel/VerticalCarousel.jsx
+++ components/verticalCarousel/VerticalCarousel.jsx
@@ -4,7 +4,12 @@ import { useState, useRef } from "react";
 import "./brand-scroller.css";
 import data from "../data/brandColumns.json";
 
-const BrandScroller = ({ brands, animationClass, hoveredImage, setHoveredImage }) => {
+const BrandScroller = ({
+  brands,
+  animationClass,
+  hoveredImage,
+  setHoveredImage,
+}) => {
   const imageRefs = useRef([]);
 
   const [pauseImage, setPauseImage] = useState(false); // default should be false
@@ -21,7 +26,11 @@ const BrandScroller = ({ brands, animationClass, hoveredImage, setHoveredImage }
 
   return (
     <div className="relative overflow-hidden h-[100vh] w-[150px] 2xl:w-[200px]">
-      <div className={`scroll-track ${animationClass} ${pauseImage ? "paused" : ""}`}>
+      <div
+        className={`scroll-track ${animationClass} ${
+          pauseImage ? "paused" : ""
+        }`}
+      >
         {[...brands, ...brands].map((brand, idx) => (
           <div
             key={`brand-${idx}`}
@@ -34,7 +43,9 @@ const BrandScroller = ({ brands, animationClass, hoveredImage, setHoveredImage }
               src={brand.src}
               alt={`brand-${idx}`}
               className={`image-item rounded-[12px] my-[20px] cursor-pointer transition-all duration-300 ${
-                hoveredImage && hoveredImage !== imageRefs.current[idx] ? "opacity-25" : "opacity-100"
+                hoveredImage && hoveredImage !== imageRefs.current[idx]
+                  ? "opacity-25"
+                  : "opacity-100"
               }`}
             />
           </div>
@@ -59,69 +70,61 @@ const VerticalCarousel = () => {
   const [hoveredImage, setHoveredImage] = useState(null);
 
   return (
-    <div className="flex gap-[16px] justify-around group w-screen 2xl:border-y border-white 2xl:overflow-hidden 2xl:h-[946px]">
-      <BrandScroller brands={brandsColumn1} animationClass="scroll-up" hoveredImage={hoveredImage} setHoveredImage={setHoveredImage} />
-      <BrandScroller brands={brandsColumn2} animationClass="scroll-down" hoveredImage={hoveredImage} setHoveredImage={setHoveredImage} />
-      <BrandScroller brands={brandsColumn3} animationClass="scroll-up" hoveredImage={hoveredImage} setHoveredImage={setHoveredImage} />
-      <BrandScroller brands={brandsColumn4} animationClass="scroll-down" hoveredImage={hoveredImage} setHoveredImage={setHoveredImage} />
-      <BrandScroller brands={brandsColumn5} animationClass="scroll-up" hoveredImage={hoveredImage} setHoveredImage={setHoveredImage} />
-      <BrandScroller brands={brandsColumn6} animationClass="scroll-down" hoveredImage={hoveredImage} setHoveredImage={setHoveredImage} />
-      <BrandScroller brands={brandsColumn7} animationClass="scroll-up" hoveredImage={hoveredImage} setHoveredImage={setHoveredImage} />
-      <BrandScroller brands={brandsColumn8} animationClass="scroll-down" hoveredImage={hoveredImage} setHoveredImage={setHoveredImage} />
+    <div className="flex justify-around gap-[16px] group w-screen 2xl:border-y border-white 2xl:overflow-hidden 2xl:h-[946px]">
+      <div className="flex gap-[16px] justify-around w-[50%]">
+        <BrandScroller
+          brands={brandsColumn1}
+          animationClass="scroll-up"
+          hoveredImage={hoveredImage}
+          setHoveredImage={setHoveredImage}
+        />
+        <BrandScroller
+          brands={brandsColumn2}
+          animationClass="scroll-down"
+          hoveredImage={hoveredImage}
+          setHoveredImage={setHoveredImage}
+        />
+        <BrandScroller
+          brands={brandsColumn3}
+          animationClass="scroll-up"
+          hoveredImage={hoveredImage}
+          setHoveredImage={setHoveredImage}
+        />
+        <BrandScroller
+          brands={brandsColumn4}
+          animationClass="scroll-down"
+          hoveredImage={hoveredImage}
+          setHoveredImage={setHoveredImage}
+        />
+      </div>
+      <div className="flex gap-[16px] justify-around w-[50%]">
+        <BrandScroller
+          brands={brandsColumn5}
+          // animationClass="scroll-up"
+          hoveredImage={hoveredImage}
+          setHoveredImage={setHoveredImage}
+        />
+        <BrandScroller
+          brands={brandsColumn6}
+          // animationClass="scroll-down"
+          hoveredImage={hoveredImage}
+          setHoveredImage={setHoveredImage}
+        />
+        <BrandScroller
+          brands={brandsColumn7}
+          // animationClass="scroll-up"
+          hoveredImage={hoveredImage}
+          setHoveredImage={setHoveredImage}
+        />
+        <BrandScroller
+          brands={brandsColumn8}
+          // animationClass="scroll-down"
+          hoveredImage={hoveredImage}
+          setHoveredImage={setHoveredImage}
+        />
+      </div>
     </div>
   );
 };
 
 export default VerticalCarousel;
-
-
-
-
-
-
-
-
-
-
-// const BrandScroller = ({ brands, animationClass }) => {
-//   const imageRefs = useRef([]);
-//   const [hoveredImage, setHoveredImage] = useState(null);
-
-//   const handleImageMouseEnter = (idx) => {
-//     setHoveredImage(imageRefs.current[idx]);
-//   };
-
-//   const handleImageMouseLeave = () => {
-//     setHoveredImage(null);
-//   };
-
-//   return (
-//     <div className="relative overflow-hidden h-[100vh] w-[150px] 2xl:w-[200px]">
-//       <div className={`scroll-track ${animationClass} ${hoveredImage ? "paused" : ""}`}>
-//         {[...brands, ...brands].map((brand, idx) => (
-//           <div
-//             key={`brand-${idx}`}
-//             className="brand-image"
-//             onMouseEnter={() => handleImageMouseEnter(idx)}
-//             onMouseLeave={handleImageMouseLeave}
-//           >
-//             <img
-//               ref={(el) => (imageRefs.current[idx] = el)}
-//               src={brand.src}
-//               alt={`brand-${idx}`}
-//               className={`image-item rounded-[12px] my-[12px] cursor-pointer transition-all duration-300 ${
-//                 hoveredImage && hoveredImage !== imageRefs.current[idx]
-//                   ? "opacity-25"
-//                   : "opacity-100"
-//               }`}
-//             />
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-
-
-
